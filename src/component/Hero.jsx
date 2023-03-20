87233d389660df381770342463d82aa315479517
Saroj Rai <99882125+sarojraii@users.noreply.github.com>
2023-03-20 19:27:56 +0545

    Update Hero.jsx

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import Typed from 'react-typed';
-
+import { Navigate } from 'react-router-dom';
 
 export default function Hero() {
+
+const [clicked, setClicked] = React.useState(false);
+
+if(clicked){
+    return<Navigate to = "/Contact"/>   
+
+}
+
+
   return (
-    <div className=' text-white'>
+    <div className=' text-white'> 
         <div className='max-w-[800px] mt-[-90px] w-full h-screen mx-auto text-center flex flex-col '>
             <p className='text-[#00df9a] mt-[200px] font-bold p-2'>WE TURN YOUR VISION INTO A POWERFUL WEBSITE</p>
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 '>Advance your website with us</h1>
@@ -19,9 +28,9 @@ export default function Hero() {
                 />
             </div>
             <div>
-                <button className=' text-black font-bold hover: cursor-pointer rounded  bg-[#00df9a] mt-3 py-2 px-5 mx-auto '>Get Started</button>
+                <button onClick={() => {setClicked(true)}} className=' text-black font-bold hover: cursor-pointer rounded  bg-[#00df9a] mt-3 py-2 px-5 mx-auto '>Get Started</button>
             </div>
         </div>
     </div>
-  )
+  );
 }
