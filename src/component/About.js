ac68a794891f9e761669783bea6f39fcb29697b9
Saroj Rai <99882125+sarojraii@users.noreply.github.com>
2023-03-20 10:15:34 +0545

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1 className='font-bold text-3xl'>About</h1>
+      <h1 className='font-bold text-3xl'>ON PROGRESS</h1>
     </div>
   )
 }
